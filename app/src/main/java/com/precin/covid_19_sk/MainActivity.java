481a8c53d64ecb3b48c0de4b68127a6ad3cda69c
Mario <37266965+majko96@users.noreply.github.com>
2020-12-16 19:04:37 +0100

    delete webview - custom towns table

--- app/src/main/java/com/precin/covid_19_sk/MainActivity.java
+++ app/src/main/java/com/precin/covid_19_sk/MainActivity.java
@@ -6,31 +6,30 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.precin.covid_19_sk.adapter.TownAdapter;
+import com.precin.covid_19_sk.model.Towns;
+
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -40,16 +39,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends Activity {
     
-    TextView txt_infected, txt_tested, txtRecovered, txtDeceased, newTotal, region1, new1, totalInfected1,dateUpdate, region2, new2, totalInfected2, region3, new3, totalInfected3, region4, new4, totalInfected4, region5, new5, totalInfected5, region6, new6, totalInfected6, region7, new7, totalInfected7, region8, new8, totalInfected8;
+    TextView txt_infected, txt_tested, txt_infectedAG, txt_testedAG, txtRecovered, txtDeceased, newTotal, region1, new1, totalInfected1,dateUpdate, region2, new2, totalInfected2, region3, new3, totalInfected3, region4, new4, totalInfected4, region5, new5, totalInfected5, region6, new6, totalInfected6, region7, new7, totalInfected7, region8, new8, totalInfected8;
     ProgressDialog pd;
-    String regionBb, newInfectedBb, totalInfectedBb, regionBa, newInfectedBa, totalInfectedBa, regionKe, newInfectedKe, totalInfectedKe, regionNr, newInfectedNr, totalInfectedNr, regionPp, newInfectedPp, totalInfectedPp,regionTn, newInfectedTn, totalInfectedTn,regionTt, newInfectedTt, totalInfectedTt,regionZa, newInfectedZa, totalInfectedZa;
-    int newBb, newBa, newKe, newNr, newPp, newTn, newTt, newZa;
-    WebView myWebView;
     private ImageButton button1;
+    private RecyclerView recyclerView;
+    private TownAdapter adapter;
+    private ArrayList<Towns> townsArrayList;
+
 
 
 
@@ -57,6 +62,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new TownAdapter(this, new ArrayList<Towns>());
+        recyclerView.setAdapter(adapter);
+        DividerItemDecoration divider = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        divider.setDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.custom_divider));
+        recyclerView.addItemDecoration(divider);
         button1 = (ImageButton) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,8 +92,6 @@ public class MainActivity extends Activity {
                                         .show();
                                 return true;
 
-                            case R.id.two:
-                                return true;
                             case R.id.three:
                                 String emailUrl = "mailto:mr.babinec@gmail.com?subject=Predmet &body= ";
                                 Intent request = new Intent(Intent.ACTION_VIEW);
@@ -100,28 +112,27 @@ public class MainActivity extends Activity {
 
 
 
-        web();
-        new JsonTask().execute("https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/LATEST?disableRedirect=true");
+        //new JsonTask().execute("https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/LATEST?disableRedirect=true");
+        new JsonTask().execute("https://melisko.online/latest.json");
         final SwipeRefreshLayout pullToRefresh = findViewById(R.id.pullToRefresh);
         pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 new JsonTask().execute("https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/LATEST?disableRedirect=true"); // your code
                 pullToRefresh.setRefreshing(false);
-                web();
+
             }
         });
 
 
-
-
         txt_infected = (TextView) findViewById(R.id.txt_infected);
         txt_tested = (TextView) findViewById(R.id.txt_tested);
+        txt_infectedAG = (TextView) findViewById(R.id.txt_infectedAG);
+        txt_testedAG = (TextView) findViewById(R.id.txt_testedAG);
         txtRecovered = (TextView) findViewById(R.id.txtRecovered);
         txtDeceased = (TextView) findViewById(R.id.txtDeceased);
         region1 = (TextView) findViewById(R.id.region1);
         new1 = (TextView) findViewById(R.id.new1);
-        newTotal = (TextView) findViewById(R.id.newTotal);
         totalInfected1 = (TextView) findViewById(R.id.totalInfected1);
         dateUpdate = (TextView) findViewById(R.id.date);
         region2 = (TextView) findViewById(R.id.region2);
@@ -148,23 +159,13 @@ public class MainActivity extends Activity {
     }
 
 
-    public void web(){
-        myWebView = findViewById(R.id.webview);
-        myWebView.loadUrl("https://mapa.covid.chat/table");
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-    }
-
-
-
 
     private class JsonTask extends AsyncTask<String, String, String> {
 
         protected void onPreExecute() {
             super.onPreExecute();
             pd = new ProgressDialog(MainActivity.this);
-            pd.setMessage("Počkajte prosím");
+            pd.setMessage("Počkajte prosím...");
             pd.setCancelable(false);
             pd.show();
         }
@@ -189,8 +190,7 @@ public class MainActivity extends Activity {
 
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line+"\n");
-                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-
+                    //Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
                 }
 
                 return buffer.toString();
@@ -219,102 +219,84 @@ public class MainActivity extends Activity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             if (pd.isShowing()){
-                pd.dismiss();
+                initView();
             }
 
             try {
 
                 JSONObject jObj = new JSONObject(result);
-                String infected = jObj.getString("infected");
-                String tested = jObj.getString("tested");
-                String recovered = jObj.getString("recovered");
-                String deceased = jObj.getString("deceased");
-                String updated = jObj.getString("updated");
 
                 JSONArray arr = jObj.getJSONArray("regionsData");
 
-                regionBb = arr.getJSONObject(0).getString("region");
-                newInfectedBb = arr.getJSONObject(0).getString("newInfected");
-                newBb = Integer.parseInt(newInfectedBb);
-                totalInfectedBb = arr.getJSONObject(0).getString("totalInfected");
+                //convert numbers to locale
+                Configuration sysConfig = getResources().getConfiguration();
+                Locale curLocale = sysConfig.locale;
+                NumberFormat nf = NumberFormat.getInstance(curLocale);
 
-                regionBa = arr.getJSONObject(1).getString("region");
-                newInfectedBa = arr.getJSONObject(1).getString("newInfected");
-                newBa = Integer.parseInt(newInfectedBa);
-                totalInfectedBa = arr.getJSONObject(1).getString("totalInfected");
+                dateUpdate.setText("Dáta zo dňa: \n" + jObj.getString("updated"));
 
-                regionKe = arr.getJSONObject(2).getString("region");
-                newInfectedKe = arr.getJSONObject(2).getString("newInfected");
-                newKe = Integer.parseInt(newInfectedKe);
-                totalInfectedKe = arr.getJSONObject(3).getString("totalInfected");
+                txtDeceased.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("deceased")))+"<sup><small>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newDeceased")))+"</small></sup>"));
+                txtRecovered.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("recovered")))+"<sup><small><font color='#00FF00'>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newRecovered")))+"</font></small></sup>"));
+                txt_tested.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("testedPCR")))+"<sup><small><font color='#FFFF00'>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newTestedAG")))+"</font></small></sup>"));
+                txt_infected.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("infectedPCR")))+"<sup><small><font color='red'>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newInfectedPCR")))+"</font></small></sup>"));
 
-                regionNr = arr.getJSONObject(3).getString("region");
-                newInfectedNr = arr.getJSONObject(3).getString("newInfected");
-                newNr = Integer.parseInt(newInfectedNr);
-                totalInfectedNr = arr.getJSONObject(3).getString("totalInfected");
+                txt_testedAG.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("testedAG")))+"<sup><small><font color='#FFFF00'>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newTestedAG")))+"</font></small></sup>"));
+                txt_infectedAG.setText(Html.fromHtml(nf.format(new Integer(jObj.getString("infectedAG")))+"<sup><small><font color='red'>\t&nbsp;+"+nf.format(new Integer(jObj.getString("newInfectedAG")))+"</font></small></sup>"));
 
-                regionPp = arr.getJSONObject(4).getString("region");
-                newInfectedPp = arr.getJSONObject(4).getString("newInfected");
-                newPp = Integer.parseInt(newInfectedPp);
-                totalInfectedPp = arr.getJSONObject(4).getString("totalInfected");
 
-                regionTn = arr.getJSONObject(5).getString("region");
-                newInfectedTn = arr.getJSONObject(5).getString("newInfected");
-                newTn = Integer.parseInt(newInfectedTn);
-                totalInfectedTn = arr.getJSONObject(5).getString("totalInfected");
+                region1.setText(arr.getJSONObject(0).getString("region"));
+                new1.setText(arr.getJSONObject(0).getString("newInfected"));
+                totalInfected1.setText(arr.getJSONObject(0).getString("totalInfected"));
+                region2.setText(arr.getJSONObject(1).getString("region"));
+                new2.setText(arr.getJSONObject(1).getString("newInfected"));
+                totalInfected2.setText(arr.getJSONObject(1).getString("totalInfected"));
+                region3.setText(arr.getJSONObject(2).getString("region"));
+                new3.setText(arr.getJSONObject(2).getString("newInfected"));
+                totalInfected3.setText(arr.getJSONObject(2).getString("totalInfected"));
+                region4.setText(arr.getJSONObject(3).getString("region"));
+                new4.setText(arr.getJSONObject(3).getString("newInfected"));
+                totalInfected4.setText(arr.getJSONObject(3).getString("totalInfected"));
+                region5.setText(arr.getJSONObject(4).getString("region"));
+                new5.setText(arr.getJSONObject(4).getString("newInfected"));
+                totalInfected5.setText(arr.getJSONObject(5).getString("totalInfected"));
+                region6.setText(arr.getJSONObject(5).getString("region"));
+                new6.setText(arr.getJSONObject(5).getString("newInfected"));
+                totalInfected6.setText(arr.getJSONObject(5).getString("totalInfected"));
+                region7.setText(arr.getJSONObject(6).getString("region"));
+                new7.setText(arr.getJSONObject(6).getString("newInfected"));
+                totalInfected7.setText(arr.getJSONObject(6).getString("totalInfected"));
+                region8.setText(arr.getJSONObject(7).getString("region"));
+                new8.setText(arr.getJSONObject(7).getString("newInfected"));
+                totalInfected8.setText(arr.getJSONObject(7).getString("totalInfected"));
 
-                regionTt = arr.getJSONObject(6).getString("region");
-                newInfectedTt = arr.getJSONObject(6).getString("newInfected");
-                newTt = Integer.parseInt(newInfectedTt);
-                totalInfectedTt = arr.getJSONObject(6).getString("totalInfected");
 
-                regionZa = arr.getJSONObject(7).getString("region");
-                newInfectedZa = arr.getJSONObject(7).getString("newInfected");
-                newZa = Integer.parseInt(newInfectedZa);
-                totalInfectedZa = arr.getJSONObject(7).getString("totalInfected");
+                JSONArray dist = jObj.getJSONArray("districts");
+                int distLen = dist.length();
+                for (int i = 0; i < distLen; i++) {
+                    Towns towns = new Towns(dist.getJSONObject(i).getString("town"), dist.getJSONObject(i).getInt("totalInfected"), dist.getJSONObject(i).getInt("newInfected"));
+                    townsArrayList.add(towns);
+                }
 
-                int totalInfected = newBb + newBa + newKe + newNr + newPp + newTn + newTt + newZa;
-                String s = String.valueOf(totalInfected);
-
-                txt_infected.setText(infected);
-                txt_tested.setText(tested);
-                txtRecovered.setText(recovered);
-                txtDeceased.setText(deceased);
-                newTotal.setText(s);
-                dateUpdate.setText("Dáta zo dňa: \n" + updated);
-                myWebView.setVisibility(View.VISIBLE);
 
             } catch (Exception e) {
                 e.printStackTrace();
                 Toast.makeText(MainActivity.this,
                         "Nepodarilo sa načítať dáta.\nSkontrolujte internetové pripojenie.", Toast.LENGTH_LONG).show();
-                myWebView.setVisibility(View.GONE);
-                // Do something to recover ... or kill the app.
             }
-                region1.setText(regionBb);
-                new1.setText(newInfectedBb);
-                totalInfected1.setText(totalInfectedBb);
-                region2.setText(regionBa);
-                new2.setText(newInfectedBa);
-                totalInfected2.setText(totalInfectedBa);
-                region3.setText(regionKe);
-                new3.setText(newInfectedKe);
-                totalInfected3.setText(totalInfectedKe);
-                region4.setText(regionNr);
-                new4.setText(newInfectedNr);
-                totalInfected4.setText(totalInfectedNr);
-                region5.setText(regionPp);
-                new5.setText(newInfectedPp);
-                totalInfected5.setText(totalInfectedPp);
-                region6.setText(regionTn);
-                new6.setText(newInfectedTn);
-                totalInfected6.setText(totalInfectedTn);
-                region7.setText(regionTt);
-                new7.setText(newInfectedTt);
-                totalInfected7.setText(totalInfectedTt);
-                region8.setText(regionZa);
-                new8.setText(newInfectedZa);
-                totalInfected8.setText(totalInfectedZa);
+
         }
+    }
+
+    private void initView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        townsArrayList = new ArrayList<>();
+        adapter = new TownAdapter(this, townsArrayList);
+        recyclerView.setAdapter(adapter);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                pd.dismiss();
+            }
+        }, 1000);
     }
 }
